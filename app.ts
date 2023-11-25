a4253bde0f9a1051bf1eba5c0e04a91649b497cc
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-25 12:04:08 +0530

    update 0.1 primitives

--- app.ts
+++ app.ts
@@ -1,14 +1,30 @@
 console.log("Type Script")
+//
+// function sum(num1: number , num2: number){
+//     return num1 + num2;
+// }
+//
+// let n1 = '10';
+// let n2 = 20;
+//
+// let value1 = sum(+n1 , +n2);
+// console.log(value1);
+//
+// let value2 = sum(+n1 , +n2);
+// console.log(value2);
 
-function sum(num1: number , num2: number){
-    return num1 + num2;
+function sendSum(num1: number , num2: number , name:string , isWithName:boolean) {
+    let result = isWithName ? `Hi  ${name}, Sum is ${num1 + num2}` : `Sum is ${isWithName}`;
+    return result;
 }
 
-let n1 = '10';
-let n2 = 20;
+let  num1: number= 10;
+let  num2: number= 20;
+let myName: string= "Tharindu";
+let isWithName: boolean= true;
 
-let value1 = sum(+n1 , +n2);
-console.log(value1);
+let result1 =  sendSum(10 , 20 , "Tharindu" , true);
+console.log(result1);
 
-let value2 = sum(+n1 , +n2);
-console.log(value2);
+let result2 =  sendSum(10 , 20 , "Tharindu" , false);
+console.log(result2);
