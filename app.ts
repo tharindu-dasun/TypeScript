567bc41c4b762f8f90aebd84e4853a6cccd2d9d7
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-25 11:42:10 +0530

    Initial commit

---
+++ app.ts
@@ -0,0 +1,14 @@
+console.log("Type Script")
+
+function sum(num1: number , num2: number){
+    return num1 + num2;
+}
+
+let n1 = '10';
+let n2 = 20;
+
+let value1 = sum(+n1 , +n2);
+console.log(value1);
+
+let value2 = sum(+n1 , +n2);
+console.log(value2);
