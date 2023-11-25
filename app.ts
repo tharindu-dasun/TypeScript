367b6289e775fe484ab2df3f9916661cb46b9430
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-25 12:10:47 +0530

    update 0.2 include return type

--- app.ts
+++ app.ts
@@ -13,18 +13,29 @@ console.log("Type Script")
 // let value2 = sum(+n1 , +n2);
 // console.log(value2);
 
-function sendSum(num1: number , num2: number , name:string , isWithName:boolean) {
-    let result = isWithName ? `Hi  ${name}, Sum is ${num1 + num2}` : `Sum is ${isWithName}`;
-    return result;
+//---------------------------------Primitives---------------------------------------------
+
+// function sendSum(num1: number , num2: number , name:string , isWithName:boolean) {
+//     let result = isWithName ? `Hi  ${name}, Sum is ${num1 + num2}` : `Sum is ${isWithName}`;
+//     return result;
+// }
+//
+// let  num1: number= 10;
+// let  num2: number= 20;
+// let myName: string= "Tharindu";
+// let isWithName: boolean= true;
+//
+// let result1 =  sendSum(10 , 20 , "Tharindu" , true);
+// console.log(result1);
+//
+// let result2 =  sendSum(10 , 20 , "Tharindu" , false);
+// console.log(result2);
+
+//---------------------------------------
+
+function sum(num1:number, num2:number): number{  // reture type eka number kiyala denawa
+    return num1 + num2;
 }
 
-let  num1: number= 10;
-let  num2: number= 20;
-let myName: string= "Tharindu";
-let isWithName: boolean= true;
-
-let result1 =  sendSum(10 , 20 , "Tharindu" , true);
-console.log(result1);
-
-let result2 =  sendSum(10 , 20 , "Tharindu" , false);
-console.log(result2);
+let results = sum(10, 20);
+console.log(`result : `, results);
