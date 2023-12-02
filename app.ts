c5a857e1d97798725ce199da54c03f455553a923
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-12-02 10:15:37 +0530

    update bank part

--- app.ts
+++ app.ts
@@ -241,16 +241,71 @@
 
 //-------------------------------------- Fixed Deposit ----------------------------
 
-let owner_name_input_elemet = document.getElementById('ownerName') as HTMLInputElement;
-let fixed_amount_input_elemet = document.getElementById('fixedAmount') as HTMLInputElement;
-let add_fix_btn_elemet = document.getElementById('addFixBtn') as ;
+let ownerName_inputElement = document.getElementById('ownerName') as HTMLInputElement;
+let fixedAmount_inputElement = document.getElementById('fixedAmount') as HTMLInputElement;
+let newInterestRate_inputElement = document.getElementById('newInterestRate') as HTMLInputElement;
 
-add_fix_btn_elemet:addEventListener('click',() =>{
-    let owner_name = owner_name_input_elemet.value;
-    let fixed_amount = fixed_amount_input_elemet.value;
+let addFix_btn = document.getElementById('addFixBtn');
+let update_interest_rate_btn_element = document.getElementById('updateInterestBtn');
+let table_body = document.getElementById('fixedDepositTableBody');
 
-    console.log('Owner Name' + owner_name);
-    console.log('fixed Amount' + fixed_amount);
+let rate = +12.5;
+let accounts:Account_Details [] = [];
 
+addFix_btn.addEventListener('click',() =>{
+    let ownerName =  ownerName_inputElement.value;
+    let fixedAmount = fixedAmount_inputElement.value;
+
+
+    let annual_interest = (+fixedAmount*12.5)/100;
+    let account =  new Account_Details(ownerName,+fixedAmount,rate,annual_interest);
+    accounts.push(account);
+    console.log(accounts);
+    updateTable();
+});
+
+function updateTable() {
+    table_body.innerHTML="";
+
+    accounts.map(r => {
+        let record_element = document.createElement('tr');
+        record_element.innerHTML = `<td>${r.owner_name}</td>
+                         <td>${r.fixed_amount}</td>
+                         <td>${r.interest_rate}</td>
+                         <td>${r.annual_interest}</td>`;
+        table_body.appendChild(record_element);
+    });
+}
+
+class Account_Details {
+    owner_name:string;
+    fixed_amount:number;
+    interest_rate:number;
+    annual_interest:number;
+
+    constructor(owner_name:string,fixed_amount:number,interest_rate:number,annual_interest:number) {
+        this.owner_name=owner_name;
+        this.fixed_amount=fixed_amount;
+        this.interest_rate=interest_rate;
+        this.annual_interest=annual_interest;
+    }
+
+    updateRate(rate: number): void{
+        this.interest_rate = rate;
+        this.annual_interest = (this.fixed_amount * rate)/100;
+    }
+}
+
+update_interest_rate_btn_element.addEventListener('click', () =>{
+    let new_Interest_rate = newInterestRate_inputElement.value;
+    rate = +new_Interest_rate;
+    let newAccounts: Account_Details[] = [];
+    accounts.map(r => {
+        newAccounts.push(r);
+        r.updateRate(rate);
+
+    });
+    accounts = newAccounts;
+    updateTable();
 });
 
