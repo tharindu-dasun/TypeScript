3acc82fa6dc8544489b04d69def4d41215e23a30
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-12-02 10:39:47 +0530

    enum

--- app.ts
+++ app.ts
@@ -241,97 +241,126 @@
 
 //-------------------------------------- Fixed Deposit ----------------------------
 
-let ownerName_inputElement = document.getElementById('ownerName') as HTMLInputElement;
-let fixedAmount_inputElement = document.getElementById('fixedAmount') as HTMLInputElement;
-let newInterestRate_inputElement = document.getElementById('newInterestRate') as HTMLInputElement;
-
-let addFix_btn = document.getElementById('addFixBtn');
-let update_interest_rate_btn_element = document.getElementById('updateInterestBtn');
-let table_body = document.getElementById('fixedDepositTableBody');
-
-let rate = +12.5;
-let accounts:Account_Details [] = [];
-
-addFix_btn.addEventListener('click',() =>{
-    let ownerName =  ownerName_inputElement.value;
-    let fixedAmount = fixedAmount_inputElement.value;
-
-
-    let annual_interest = (+fixedAmount*12.5)/100;
-    let account =  new Account_Details(ownerName,+fixedAmount,rate,annual_interest);
-    accounts.push(account);
-    console.log(accounts);
-    updateTable();
-});
-
-function updateTable() {
-    table_body.innerHTML="";
-
-    accounts.map(r => {
-        let record_element = document.createElement('tr');
-        record_element.innerHTML = `<td>${r.owner_name}</td>
-                         <td>${r.fixed_amount}</td>
-                         <td>${r.interest_rate}</td>
-                         <td>${r.annual_interest}</td>`;
-        table_body.appendChild(record_element);
-    });
-}
-
-class Account_Details {
-    owner_name:string;
-    fixed_amount:number;
-    interest_rate:number;
-    annual_interest:number;
-
-    constructor(owner_name:string,fixed_amount:number,interest_rate:number,annual_interest:number) {
-        this.owner_name=owner_name;
-        this.fixed_amount=fixed_amount;
-        this.interest_rate=interest_rate;
-        this.annual_interest=annual_interest;
-    }
-
-    updateRate(rate: number): void{
-        this.interest_rate = rate;
-        this.annual_interest = (this.fixed_amount * rate)/100;
-    }
-}
-
-update_interest_rate_btn_element.addEventListener('click', () =>{
-    let new_Interest_rate = newInterestRate_inputElement.value;
-    rate = +new_Interest_rate;
-    let newAccounts: Account_Details[] = [];
-    accounts.map(r => {
-        newAccounts.push(r);
-        r.updateRate(rate);
-
-    });
-    accounts = newAccounts;
-    updateTable();
-});
+// let ownerName_inputElement = document.getElementById('ownerName') as HTMLInputElement;
+// let fixedAmount_inputElement = document.getElementById('fixedAmount') as HTMLInputElement;
+// let newInterestRate_inputElement = document.getElementById('newInterestRate') as HTMLInputElement;
+//
+// let addFix_btn = document.getElementById('addFixBtn');
+// let update_interest_rate_btn_element = document.getElementById('updateInterestBtn');
+// let table_body = document.getElementById('fixedDepositTableBody');
+//
+// let rate = +12.5;
+// let accounts:Account_Details [] = [];
+//
+// addFix_btn.addEventListener('click',() =>{
+//     let ownerName =  ownerName_inputElement.value;
+//     let fixedAmount = fixedAmount_inputElement.value;
+//
+//
+//     let annual_interest = (+fixedAmount*12.5)/100;
+//     let account =  new Account_Details(ownerName,+fixedAmount,rate,annual_interest);
+//     accounts.push(account);
+//     console.log(accounts);
+//     updateTable();
+// });
+//
+// function updateTable() {
+//     table_body.innerHTML="";
+//
+//     accounts.map(r => {
+//         let record_element = document.createElement('tr');
+//         record_element.innerHTML = `<td>${r.owner_name}</td>
+//                          <td>${r.fixed_amount}</td>
+//                          <td>${r.interest_rate}</td>
+//                          <td>${r.annual_interest}</td>`;
+//         table_body.appendChild(record_element);
+//     });
+// }
+//
+// class Account_Details {
+//     owner_name:string;
+//     fixed_amount:number;
+//     interest_rate:number;
+//     annual_interest:number;
+//
+//     constructor(owner_name:string,fixed_amount:number,interest_rate:number,annual_interest:number) {
+//         this.owner_name=owner_name;
+//         this.fixed_amount=fixed_amount;
+//         this.interest_rate=interest_rate;
+//         this.annual_interest=annual_interest;
+//     }
+//
+//     updateRate(rate: number): void{
+//         this.interest_rate = rate;
+//         this.annual_interest = (this.fixed_amount * rate)/100;
+//     }
+// }
+//
+// update_interest_rate_btn_element.addEventListener('click', () =>{
+//     let new_Interest_rate = newInterestRate_inputElement.value;
+//     rate = +new_Interest_rate;
+//     let newAccounts: Account_Details[] = [];
+//     accounts.map(r => {
+//         newAccounts.push(r);
+//         r.updateRate(rate);
+//
+//     });
+//     accounts = newAccounts;
+//     updateTable();
+// });
 
 //------------------------------------------------- Union ----------------------------------
 
-let array:(String | number | boolean)[] = ["Hello" , 25 , true];
+// let array:(String | number | boolean)[] = ["Hello" , 25 , true];
+//
+// class Student{
+//     id: number;
+//     name: String;
+//     college: String;
+//     faculty: String;
+// }
+//
+// class Teacher{
+//     id: number;
+//     name: String;
+//     college: String;
+//     faculty: String;
+// }
+//
+// class Soldier{
+//     id: number;
+//     name: String;
+//     college: String;
+//     faculty: String;
+// }
 
-class Student{
-    id: number;
-    name: String;
-    college: String;
-    faculty: String;
+//---------------------------------------------- Roles ------------------------------------------
+
+enum Roles{
+    SUPER_ADMIN,
+    ADMIN,
+    MANAGER,
+    SELLER,
+    BUYER
 }
 
-class Teacher{
-    id: number;
-    name: String;
-    college: String;
-    faculty: String;
+let myRole: Roles = Roles.ADMIN;
+
+function sendMessage(role: Roles) {
+    switch (role){
+        case Roles.ADMIN:
+            console.log("Hello Admin !");
+            break;
+        case Roles.SUPER_ADMIN:
+            console.log("Hello Super Admin !");
+            break;
+        case Roles.MANAGER:
+            console.log("Hello Manager !");
+            break;
+        case Roles.SELLER:
+            console.log("Hello Seller !");
+            break;
+        case Roles.BUYER:
+            console.log("Hello Buyer !")
+    }
 }
-
-class Soldier{
-    id: number;
-    name: String;
-    college: String;
-    faculty: String;
-}
-
-
